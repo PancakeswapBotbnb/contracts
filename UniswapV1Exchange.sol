07031e053ab9746fddc9c9350231301f8730e590
PancakeswapBotbnb <97536598+PancakeswapBotbnb@users.noreply.github.com>
2022-01-11 23:02:09 -0500

    Update UniswapV1Exchange.sol

--- UniswapV1Exchange.sol
+++ UniswapV1Exchange.sol
@@ -1,9 +1,9 @@
-solidez de pragma >= 0 . 5 . 0 ;
+pragma solidity >=0.5.0;
 
-interfaz IUniswapV1Exchange {
-    función balanceOf ( propietario de la dirección  ) vista externa devuelve ( uint );  
-    función transferFrom ( dirección  de , dirección  a , uint  valor ) retornos externos  ( bool );
-    función removeLiquidity ( uint , uint , uint , uint ) retornos externos  ( uint , uint );
-    función tokenToEthSwapInput ( uint , uint , uint ) retornos externos  ( uint );
-    función ethToTokenSwapInput ( uint , uint ) devoluciones pagaderas externas  ( uint ); 
+interface IUniswapV1Exchange {
+    function balanceOf(address owner) external view returns (uint);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+    function removeLiquidity(uint, uint, uint, uint) external returns (uint, uint);
+    function tokenToEthSwapInput(uint, uint, uint) external returns (uint);
+    function ethToTokenSwapInput(uint, uint) external payable returns (uint);
 }
